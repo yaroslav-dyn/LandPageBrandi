e577348895e5a6e4614398151b376d95b70b314f
yaroslav-dyn <norman_zp@outlook.com>
2016-05-12 09:30:03 +0300

    110516

--- js/user.js
+++ js/user.js
@@ -8,10 +8,21 @@ $( document ).ready(function() {
 
 	//Gallery team
 	$('.full-item-team').on('click', function(){
-      $(this).find('.item-gall-hidden').toggleClass('hidden');
-      $(this).find('.pf-name').toggleClass('active-green');
+		$(this).find('.item-gall-hidden').toggleClass('hidden');
+		$(this).find('.pf-name').toggleClass('active-green');
 
 	});
 
+	$(window).on('scroll', function(){
+		var docSctop = $(this).scrollTop();
+		console.log(docSctop);
+		if(docSctop >= 3300 && docSctop <= 3980){
+			$('.header-top').css('background', 'rgba(255,255,255,0.2');
+		}
+		else{
+			$('.header-top').css('background', 'rgba(16,22,54, 0.2');
+		}
+	});
 
 });
+
