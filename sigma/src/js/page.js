2aaa473497ef5d570a740149ef9833c8499cdbc8
yaroslav-dyn <norman_zp@outlook.com>
2016-06-29 18:35:39 +0300

    new ui

--- sigma/src/js/page.js
+++ sigma/src/js/page.js
@@ -29,7 +29,7 @@ function fileAppStare(){
 
 
 
-//  parse file in area
+//  parse file in area (sigma.js)
 function receivedText() {
     var nameFile = fr.result;
     var gexfSigma = new sigma({
@@ -71,110 +71,3 @@ $('#upload-input').on('change',function(){
     nameEl.addClass('name-show');
 
 });
-//*******************************************************************************************
-
-var g = {
-    "nodes": [
-        {
-            "id": "n0",
-            "label": "node 1",
-            "x": 0,
-            "y": 0,
-            'size': 1
-        },
-        {
-            "id": "n1",
-            "label": "node 2",
-            "x": 3,
-            "y": 0,
-            'size': 1
-        },
-        {
-            "id": "n2",
-            "label": "node 3",
-            "x": 3,
-            "y": 3,
-            'size': 1
-        },
-        {
-            "id": "n3",
-            "label": "node 4",
-            "x": 0,
-            "y": 3,
-            'size': 1
-        },
-        {
-            "id": "n4",
-            "label": "node 4",
-            "x": 1.5,
-            "y": 1.5,
-            'size': 1
-        }
-    ],
-    "edges": [
-        {
-            "id": "e0",
-            "source": "n0",
-            "target": "n1"
-        },
-        {
-            "id": "e1",
-            "source": "n1",
-            "target": "n2"
-        },
-        {
-            "id": "e2",
-            "source": "n2",
-            "target": "n3"
-        },
-        {
-            "id": "e3",
-            "source": "n3",
-            "target": "n0"
-        },
-        {
-            "id": "e4",
-            "source": "n0",
-            "target": "n2"
-        },
-        {
-            "id": "e5",
-            "source": "n1",
-            "target": "n3"
-        }
-    ]
-};
-
-
-
-
-var s = new sigma({
-    graph: g,
-    renderer: {
-        container: 'graph-container',
-        type: 'canvas'
-    },
-    settings: {
-        minEdgeSize: 1,
-        maxEdgeSize: 3,
-        minNodeSize: 2,
-        maxNodeSize: 3,
-        defaultEdgeColor: 'purple',
-        edgeColor: 'edge',
-        defaultNodeColor: 'red',
-        nodeColor: 'node',
-        font: "Verdana",
-        labelThreshold: "3",
-        defaultLabelColor:"green"
-    }
-});
-
-
-
-
-
-
-
-
-//// Finally, let's ask our sigma instance to refresh:
-//s.refresh();
