b132125d6740d5331bce4f7c862f0aec86849938
yaroslav-dyn <norman_zp@outlook.com>
2016-07-02 17:18:32 +0300

    design

--- sigma/src/js/page.js
+++ sigma/src/js/page.js
@@ -3,7 +3,6 @@ var input = document.getElementById('upload-input');
 //rules to upload file
 function handleFileSelect()
 {
-
     if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
         alert('The File APIs are not fully supported in this browser.');
         return;
@@ -11,7 +10,6 @@ function handleFileSelect()
     else if (!input.files) {
         alert("This browser doesn't seem to support the `files` property of file inputs.");
     }
-
     else {
         fileAppStare();
     }
@@ -26,7 +24,6 @@ function fileAppStare(){
     //fr.readAsText(file);
     fr.readAsDataURL(file);
 }
-
 
 
 //  parse file in area (sigma.js)
@@ -52,11 +49,47 @@ function receivedText() {
         gexfSigma,
         function() {
             gexfSigma.refresh();
+            gexfSigma.graph.nodes().forEach(function (n) {  
+                if(n.attributes[0] != undefined){
+                    var el = n.attributes[0];
+                }
+                else{
+                    console.log("attribute not found")
+                }
+              
+            })
         }
+               // Array.prototype.contains = function(v) {
+               //      for(var i = 0; i < this.length; i++) {
+               //          if(this[i] === v) return true;
+               //      }
+               //      return false;
+               //  };
+
+               //  Array.prototype.unique = function() {
+               //      var arr = [];
+               //      for(var i = 0; i < this.length; i++) {
+               //          if(!arr.contains(this[i])) {
+               //              arr.push(this[i]);
+               //          }
+               //      }
+               //      return arr; 
+               //  }
+               //  var uniqueMas = n.attributes[0].unique()
+
+
 
     );
+    //export json
+    //$('#pj').click(function(){
+    //    gexfSigma.toJSON({
+    //        download: true,
+    //        filename: 'graph.json'
+    //    });
+    //})
 
-}
+}//End parse file in area (sigma.js)
+
 
 //On change do all
 $('#upload-input').on('change',function(){
@@ -66,8 +99,21 @@ $('#upload-input').on('change',function(){
     var spanEl = document.getElementById('header-file');
     fileName.innerText = "file Upload: ";
     spanEl.innerText = parseName;
-
     var nameEl = $('.load-file-name');
     nameEl.addClass('name-show');
+    $('#container-gexf').height(400);
+ 
+    //snapshot
+    $('#pj').on('click', function(){
+        html2canvas([ document.getElementById('container-gexf') ], {
+            onrendered: function(canvas) {
+               $('#snap-area').append(canvas);
+            }
+        });
+    });
+
 
 });
+
+
+
