0fc8b5fcb0d6d68b38f19f8b93f2566b4d1519e0
yaroslav-dyn <norman_zp@outlook.com>
2016-07-08 11:03:27 +0300

    try filters

--- sigma/gulpfile.js
+++ sigma/gulpfile.js
@@ -3,6 +3,7 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var useref = require('gulp-useref');
 var del  = require('del');
+var rigger = require('gulp-rigger');
 
 
 gulp.task('sass', function () {
@@ -32,6 +33,19 @@ gulp.task('img-build', function () {
     .pipe(gulp.dest('public/img/'));
 });
 
+//rigger html
+gulp.task('rigger-html', function () {
+    gulp.src('src/html-parts/src/*.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('src/'));
+});
+//rigger js
+gulp.task('rigger-js', function () {
+    gulp.src('src/js/parts/main.js')
+        .pipe(rigger())
+        .pipe(gulp.dest('src/js/'));
+});
+
 //all build
 gulp.task('build', 
 [
@@ -49,15 +63,15 @@ gulp.task('build-clean', function(){
 
 // Static Server + watching css/html files
 gulp.task('serv', function() {
-
     browserSync.init({
         server: "./src/"
     });
-    gulp.watch('src/**/*.html').on('change', browserSync.reload);
-    gulp.watch('src/js/*.js' ).on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js',['rigger-js'] );
+    gulp.watch('src/js/*.js').on('change', browserSync.reload);
     gulp.watch('src/scss/*.scss',['sass']);
+    gulp.watch('src/html-parts/**/*.html',['rigger-html']);
+    gulp.watch('src/*.html').on('change', browserSync.reload);
     gulp.watch('src/css/*.css' ).on('change', browserSync.reload);
-
 });
 
 
--- sigma/src/js/parts/filters.js
+++ sigma/src/js/parts/filters.js
@@ -52,45 +52,36 @@ var _ = {
   }
 };
 function updatePane (graph, filter) {
-  // get max degree
-  var maxDegree = 0,
-      categories = {};
+  var categories = {};
+
   // read nodes
   graph.nodes().forEach(function(n) {
-    maxDegree = Math.max(maxDegree, graph.degree(n.id));
     categories[n.attributes.cat] = true;
-  })
-  // min degree
-  _.$('min-degree').max = maxDegree;
-  _.$('max-degree-value').textContent = maxDegree;
-  // node category
-  var nodecategoryElt = _.$('node-category');
+  });
+
+  var nodecategoryElt = $('#node-category');
   Object.keys(categories).forEach(function(c) {
-    var optionElt = document.createElement("option");
+    var optionElt = document.createElement("li");
     optionElt.text = c;
-    nodecategoryElt.add(optionElt);
+    console.log(optionElt.text);
+    nodecategoryElt.append("<li>"+optionElt.text +"</li>");
   });
   // reset button
   _.$('reset-btn').addEventListener("click", function(e) {
-    _.$('min-degree').value = 0;
-    _.$('min-degree-val').textContent = '0';
     _.$('node-category').selectedIndex = 0;
     filter.undo().apply();
     _.$('dump').textContent = '';
     _.hide('#dump');
   });
-  // export button
-  _.$('export-btn').addEventListener("click", function(e) {
-    var chain = filter.serialize();
-    console.log(chain);
-    _.$('dump').textContent = JSON.stringify(chain);
-    _.show('#dump');
-  });
+  //// export button
+  //_.$('export-btn').addEventListener("click", function(e) {
+  //  var chain = filter.serialize();
+  //  console.log(chain);
+  //  _.$('dump').textContent = JSON.stringify(chain);
+  //  _.show('#dump');
+  //});
 }
 // Initialize sigma with the dataset:
-//   e-Diaspora Moroccan corpus of websites
-//   by Dana Diminescu & Matthieu Renault
-//   http://www.e-diasporas.fr/wp/moroccan.html
 sigma.parsers.gexf('gexf/graph-4.gexf', {
   container: 'container-gexf',
   settings: {
@@ -101,22 +92,6 @@ sigma.parsers.gexf('gexf/graph-4.gexf', {
   // Initialize the Filter API
   filter = sigma.plugins.filter(s);
   updatePane(s.graph, filter);
-  function applyMinDegreeFilter(e) {
-    var v = e.target.value;
-    _.$('min-degree-val').textContent = v;
-    filter
-      .undo('min-degree')
-      .nodesBy(
-        function(n, options) {
-          return this.graph.degree(n.id) >= options.minDegreeVal;
-        },
-        {
-          minDegreeVal: +v
-        },
-        'min-degree'
-      )
-      .apply();
-  }
   function applyCategoryFilter(e) {
     var c = e.target[e.target.selectedIndex].value;
     filter
@@ -132,7 +107,6 @@ sigma.parsers.gexf('gexf/graph-4.gexf', {
       )
       .apply();
   }
-  _.$('min-degree').addEventListener("input", applyMinDegreeFilter);  // for Chrome and FF
-  _.$('min-degree').addEventListener("change", applyMinDegreeFilter); // for IE10+, that sucks
+
   _.$('node-category').addEventListener("change", applyCategoryFilter);
 });
